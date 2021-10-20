88b7983231b0557535335e091a832e1f5e4afe3d
HUGO ANAYA <hugo.anaya@live.com>
2021-10-19 23:11:50 -0500

    Últimos cambios del 19/10/2021
    CSS:
    Se adaptaron los estilos a las clases creadas por Franky.
    Se quitaron algunas líneas de código que ya no se iban a volver a usar.
    
    INDEX:
    Había doble menú, se comentó el ultimo.
    Se cambiaron algunos nombres de clases como en la 106 para poder usar el JS.
    Se agregó el título de la seccion.
    Se reestableció el orden de doble tallerista por fila.
    
    JS:
    Se agregaron las funciones para el diseño por Hugo.  y más cositas

--- js/script.js
+++ js/script.js
@@ -1,79 +1,187 @@
-var btn1 = document.getElementById('Sbtn1'),
-    btn2 = document.getElementById('Sbtn2'),
-    btn3 = document.getElementById('Sbtn3'),
-    btn4 = document.getElementById('Sbtn4'),
-    cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0;
+var btn1 = document.getElementById("Sbtn1"),
+  btn2 = document.getElementById("Sbtn2"),
+  btn3 = document.getElementById("Sbtn3"),
+  btn4 = document.getElementById("Sbtn4"),
+  cont1 = 0,
+  cont2 = 0,
+  cont3 = 0,
+  cont4 = 0;
 
-document.getElementById('TSem1').style.display = "none";
-document.getElementById('TSem3').style.display = "none";
-document.getElementById('TSem5').style.display = "none";
-document.getElementById('TSem7').style.display = "none";
+document.getElementById("TSem1").style.display = "none";
+document.getElementById("TSem3").style.display = "none";
+document.getElementById("TSem5").style.display = "none";
+document.getElementById("TSem7").style.display = "none";
 
-btn1.addEventListener('click', mostrarS1, true);
-btn2.addEventListener('click', mostrarS3, true);
-btn3.addEventListener('click', mostrarS5, true);
-btn4.addEventListener('click', mostrarS7, true);
+btn1.addEventListener("click", mostrarS1, true);
+btn2.addEventListener("click", mostrarS3, true);
+btn3.addEventListener("click", mostrarS5, true);
+btn4.addEventListener("click", mostrarS7, true);
 
 function mostrarS1() {
-    if (cont1 == 0) {
-        document.getElementById('TSem1').style.display = "block";
-        document.getElementById('TSem3').style.display = "none";
-        document.getElementById('TSem5').style.display = "none";
-        document.getElementById('TSem7').style.display = "none";
-        cont1 = 1;
-        cont2 = 0;
-        cont3 = 0;
-        cont4 = 0;
-    } else {
-        document.getElementById('TSem1').style.display = "none";
-        cont1 = 0;
-    }
-
+  if (cont1 == 0) {
+    btn1ON();
+    document.getElementById("TSem1").style.display = "block";
+    document.getElementById("TSem3").style.display = "none";
+    document.getElementById("TSem5").style.display = "none";
+    document.getElementById("TSem7").style.display = "none";
+    cont1 = 1;
+    cont2 = 0;
+    cont3 = 0;
+    cont4 = 0;
+  } else {
+    btnOFF();
+    document.getElementById("TSem1").style.display = "none";
+    cont1 = 0;
+  }
 }
 function mostrarS3() {
-    if (cont2 == 0) {
-        document.getElementById('TSem3').style.display = "block";
-        document.getElementById('TSem1').style.display = "none";
-        document.getElementById('TSem5').style.display = "none";
-        document.getElementById('TSem7').style.display = "none";
-        cont2 = 1;
-        cont1 = 0;
-        cont3 = 0;
-        cont4 = 0;
-    } else {
-        document.getElementById('TSem3').style.display = "none";
-        cont2 = 0;
-    }
+  if (cont2 == 0) {
+    btn3ON();
+    document.getElementById("TSem3").style.display = "block";
+    document.getElementById("TSem1").style.display = "none";
+    document.getElementById("TSem5").style.display = "none";
+    document.getElementById("TSem7").style.display = "none";
+    cont2 = 1;
+    cont1 = 0;
+    cont3 = 0;
+    cont4 = 0;
+  } else {
+    btnOFF();
+    document.getElementById("TSem3").style.display = "none";
+    cont2 = 0;
+  }
 }
 function mostrarS5() {
-    if (cont3 == 0) {
-        document.getElementById('TSem5').style.display = "block";
-        document.getElementById('TSem3').style.display = "none";
-        document.getElementById('TSem1').style.display = "none";
-        document.getElementById('TSem7').style.display = "none";
-        cont3 = 1;
-        cont1 = 0;
-        cont2 = 0;
-        cont4 = 0;
-    } else {
-        document.getElementById('TSem5').style.display = "none";
-        cont3 = 0;
-
-    }
-
+  if (cont3 == 0) {
+    btn5ON();
+    document.getElementById("TSem5").style.display = "block";
+    document.getElementById("TSem3").style.display = "none";
+    document.getElementById("TSem1").style.display = "none";
+    document.getElementById("TSem7").style.display = "none";
+    cont3 = 1;
+    cont1 = 0;
+    cont2 = 0;
+    cont4 = 0;
+  } else {
+    btnOFF();
+    document.getElementById("TSem5").style.display = "none";
+    cont3 = 0;
+  }
 }
 function mostrarS7() {
-    if (cont4 == 0) {
-        document.getElementById('TSem7').style.display = "block";
-        document.getElementById('TSem3').style.display = "none";
-        document.getElementById('TSem1').style.display = "none";
-        document.getElementById('TSem5').style.display = "none";
-        cont4 = 1;
-        cont1 = 0;
-        cont3 = 0;
-        cont2 = 0;
-    } else {
-        document.getElementById('TSem7').style.display = "none";
-        cont4 = 0;
-    }
+  if (cont4 == 0) {
+    btn7ON();
+    document.getElementById("TSem7").style.display = "block";
+    document.getElementById("TSem3").style.display = "none";
+    document.getElementById("TSem1").style.display = "none";
+    document.getElementById("TSem5").style.display = "none";
+    cont4 = 1;
+    cont1 = 0;
+    cont3 = 0;
+    cont2 = 0;
+  } else {
+    btnOFF();
+    document.getElementById("TSem7").style.display = "none";
+    cont4 = 0;
+  }
+}
+
+function btnOFF() {
+  // Btn1
+  document.getElementById("Sbtn1").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn1").style.color = "#858585";
+  document.getElementById("Sbtn1").style.borderColor = "#fff";
+  document.getElementById("OculTSection").style.borderBottomColor = "#3D3A6C";
+  // Btn3
+  document.getElementById("Sbtn2").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn2").style.color = "#858585";
+  document.getElementById("Sbtn2").style.borderColor = "#fff";
+  // Btn5
+  document.getElementById("Sbtn3").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn3").style.color = "#858585";
+  document.getElementById("Sbtn3").style.borderColor = "#fff";
+  // Btn7
+  document.getElementById("Sbtn4").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn4").style.color = "#858585";
+  document.getElementById("Sbtn4").style.borderColor = "#fff";
+}
+
+function btn1ON() {
+  // Btn1
+  document.getElementById("Sbtn1").style.backgroundColor = "#F1F6FA";
+  document.getElementById("Sbtn1").style.color = "#222222";
+  document.getElementById("Sbtn1").style.borderColor = "#F1F6FA";
+  document.getElementById("OculTSection").style.borderBottomColor = "#F1F6FA";
+  // Btn3
+  document.getElementById("Sbtn2").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn2").style.color = "#858585";
+  document.getElementById("Sbtn2").style.borderColor = "#fff";
+  // Btn5
+  document.getElementById("Sbtn3").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn3").style.color = "#858585";
+  document.getElementById("Sbtn3").style.borderColor = "#fff";
+  // Btn7
+  document.getElementById("Sbtn4").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn4").style.color = "#858585";
+  document.getElementById("Sbtn4").style.borderColor = "#fff";
+}
+
+function btn3ON() {
+  // Btn3
+  document.getElementById("Sbtn2").style.backgroundColor = "#F1F6FA";
+  document.getElementById("Sbtn2").style.color = "#222222";
+  document.getElementById("Sbtn2").style.borderColor = "#F1F6FA";
+  document.getElementById("OculTSection").style.borderBottomColor = "#F1F6FA";
+  // Btn1
+  document.getElementById("Sbtn1").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn1").style.color = "#858585";
+  document.getElementById("Sbtn1").style.borderColor = "#fff";
+  // Btn5
+  document.getElementById("Sbtn3").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn3").style.color = "#858585";
+  document.getElementById("Sbtn3").style.borderColor = "#fff";
+  // Btn7
+  document.getElementById("Sbtn4").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn4").style.color = "#858585";
+  document.getElementById("Sbtn4").style.borderColor = "#fff";
+}
+
+function btn5ON() {
+  // Btn5
+  document.getElementById("Sbtn3").style.backgroundColor = "#F1F6FA";
+  document.getElementById("Sbtn3").style.color = "#222222";
+  document.getElementById("Sbtn3").style.borderColor = "#F1F6FA";
+  document.getElementById("OculTSection").style.borderBottomColor = "#F1F6FA";
+  // Btn3
+  document.getElementById("Sbtn2").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn2").style.color = "#858585";
+  document.getElementById("Sbtn2").style.borderColor = "#fff";
+  // Btn1
+  document.getElementById("Sbtn1").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn1").style.color = "#858585";
+  document.getElementById("Sbtn1").style.borderColor = "#fff";
+  // Btn7
+  document.getElementById("Sbtn4").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn4").style.color = "#858585";
+  document.getElementById("Sbtn4").style.borderColor = "#fff";
+}
+
+function btn7ON() {
+  // Btn7
+  document.getElementById("Sbtn4").style.backgroundColor = "#F1F6FA";
+  document.getElementById("Sbtn4").style.color = "#222222";
+  document.getElementById("Sbtn4").style.borderColor = "#F1F6FA";
+  document.getElementById("OculTSection").style.borderBottomColor = "#F1F6FA";
+  // Btn3
+  document.getElementById("Sbtn2").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn2").style.color = "#858585";
+  document.getElementById("Sbtn2").style.borderColor = "#fff";
+  // Btn5
+  document.getElementById("Sbtn3").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn3").style.color = "#858585";
+  document.getElementById("Sbtn3").style.borderColor = "#fff";
+  // Btn1
+  document.getElementById("Sbtn1").style.backgroundColor = "#fff";
+  document.getElementById("Sbtn1").style.color = "#858585";
+  document.getElementById("Sbtn1").style.borderColor = "#fff";
 }
