913506f4222e562e8839669b941519798ab774f7
HUGO ANAYA <hugo.anaya@live.com>
2021-10-22 12:36:47 -0500

    CAMBIOS MUY IMPORTANTES / TARJETAS AUTOMATICAS CON TIMER
    CSS:
    - Se integró los estilos visuales de las tarjetas  | Frank
    - Se agregaron varias funciones de estilos para los filtros y varias correcciones pendientes.
    - Algunos títulos de secciones en comentarios, las sobresalté más.
    
    Index.html:
    - Se aplicó autoformato para verse mejor.
    - Se quitó el menú generado automáticamente al sincronizar que estaba fuera de lugar.
    - Se agregaron muchos ID.
    - Se quitó del menu de días la parte de los semestres y se puso el menú a días.
    - Se agregaron todos los talleristas de todos los días con su información tal cual al documento enviado en Excel.
    - Se agregaron muchos ID para las tarjetas y cada uno de sus elementos a utilizar.
    - Se quitó varios div del boton de acceso para poder ser enviado desde el JS.
    - Se quitó un footer que estaba de más.
     JavaScript:
    - Se agregó un nuevo file JS llamado countdown.js, este es la base del reloj, aunque no cumple una función útil como tal porque todo el codigo que contiene, está en el script principal.
    - Se agregaron las constantes de links en la parte superior del documento para cuando nos pasen los links de los talleres y no tener que reemplazarlos en diversas partes del código.
    - Se comentaron algunas funciones de el método de talleres anterior.
    - Se agregó el código del countdown.js a partir de la linea 276.
    - Se tienen cuatro funciones de tiempo, una para cada tarjeta de taller, en este caso solo se implementó para el lunes.
    - Dentro de cada función se agregaron actividades como: al estar en vivo se cambia el color del botón, el texto se cambia, el div se reemplaza por un "<a>" para que el botón sea un link; Al momento de que se acabe el tiempo, se cambia nuevamente de <a> a <div>, se cambia el texto a finalizado, se quita la parte de envivo, se agrega una clase al div para que este se vea a blanco y negro, se pone el cursor en prohibido.
    - Se colocaron countdowns de prueba.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// LINKS DE ZOOM
+const linkLu1 = "http://google.com",
+  LinkLu2 = "http://facebook.com",
+  LinkLu3 = "http://google.com",
+  LinkLu4 = "http://google.com",
+  LinkMar1 = "http://google.com",
+  LinkMar2 = "http://google.com",
+  LinkMar3 = "",
+  LinkMar4 = "",
+  LinkMie1 = "",
+  LinkMie2 = "",
+  LinkMie3 = "",
+  LinkMie4 = "",
+  LinkJue1 = "",
+  LinkJue2 = "",
+  LinkVie1 = "",
+  LinkVie2 = "",
+  LinkVie3 = "";
 var btn1 = document.getElementById("Sbtn1"),
   btn2 = document.getElementById("Sbtn2"),
   btn3 = document.getElementById("Sbtn3"),
@@ -9,10 +27,10 @@ var btn1 = document.getElementById("Sbtn1"),
   cont4 = 0,
   cont5 = 0;
 
-document.getElementById("TSem1").style.display = "none";
-document.getElementById("TSem3").style.display = "none";
-document.getElementById("TSem5").style.display = "none";
-document.getElementById("TSem7").style.display = "none";
+// document.getElementById("TSem1").style.display = "none";
+// document.getElementById("TSem3").style.display = "none";
+// document.getElementById("TSem5").style.display = "none";
+// document.getElementById("TSem7").style.display = "none";
 
 btn1.addEventListener("click", mostrarLu, true);
 btn2.addEventListener("click", mostrarMa, true);
@@ -112,7 +130,8 @@ function btnOFF() {
   document.getElementById("Sbtn1").style.backgroundColor = "#fff";
   document.getElementById("Sbtn1").style.color = "#3D3A6C";
   document.getElementById("Sbtn1").style.borderColor = "#fff";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
   // Martes 16
   document.getElementById("Sbtn2").style.backgroundColor = "#fff";
   document.getElementById("Sbtn2").style.color = "#3D3A6C";
@@ -136,7 +155,8 @@ function btnLunesON() {
   document.getElementById("Sbtn1").style.backgroundColor = "#F0F6FA";
   document.getElementById("Sbtn1").style.color = "#222";
   document.getElementById("Sbtn1").style.borderColor = "#F0F6FA";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
   // Martes 16
   document.getElementById("Sbtn2").style.backgroundColor = "#fff";
   document.getElementById("Sbtn2").style.color = "#3D3A6C";
@@ -164,7 +184,8 @@ function btnMartesON() {
   document.getElementById("Sbtn2").style.backgroundColor = "#F0F6FA";
   document.getElementById("Sbtn2").style.color = "#222222";
   document.getElementById("Sbtn2").style.borderColor = "#F0F6FA";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
   // Miercoles 17
   document.getElementById("Sbtn3").style.backgroundColor = "#fff";
   document.getElementById("Sbtn3").style.color = "#3D3A6C";
@@ -192,7 +213,8 @@ function btnMiercolesON() {
   document.getElementById("Sbtn3").style.backgroundColor = "#F0F6FA";
   document.getElementById("Sbtn3").style.color = "#222222";
   document.getElementById("Sbtn3").style.borderColor = "#F0F6FA";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
   // Jueves 18
   document.getElementById("Sbtn4").style.backgroundColor = "#fff";
   document.getElementById("Sbtn4").style.color = "#3D3A6C";
@@ -220,7 +242,8 @@ function btnJuevesON() {
   document.getElementById("Sbtn4").style.backgroundColor = "#F0F6FA";
   document.getElementById("Sbtn4").style.color = "#222222";
   document.getElementById("Sbtn4").style.borderColor = "#F0F6FA";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
   // Viernes 19
   document.getElementById("Sbtn5").style.backgroundColor = "#fff";
   document.getElementById("Sbtn5").style.color = "#3D3A6C";
@@ -247,9 +270,235 @@ function btnViernesON() {
   document.getElementById("Sbtn5").style.backgroundColor = "#F0F6FA";
   document.getElementById("Sbtn5").style.color = "#222222";
   document.getElementById("Sbtn5").style.borderColor = "#F0F6FA";
-  document.getElementById("contenedorBtnSemestre").style.borderBottomColor = "#707070";
+  document.getElementById("contenedorBtnSemestre").style.borderBottomColor =
+    "#707070";
 }
 
+// TIMER Y COUNTDOWN
+
+// Reloj y conversiones para el formato actual
+const getRemainingTime = (deadline) => {
+  let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
+
+  return {
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+    remainTime,
+  };
+};
+
+// COUNTDOWNS DE CADA TALLER
+
+const btnStateLu1 = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
+
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}d: ${t.remainHours}h: ${t.remainMinutes}m: ${t.remainSeconds}s `;
+    // EN VIVO
+    if (t.remainTime <= 1) {
+      console.log(t.remainTime);
+      // clearInterval(timerUpdate);
+      // el.innerHTML = finalMessage;
+      //   el.innerHTML = `<a href="http://itsm.edu.mx" target="_blank"><div>
+      //   <p>Ingresar</p></div>
+      // </a>`;
+      el.innerHTML = `<p> Acceder <p>`;
+      document.getElementById("liveIDLu1").style.display = "flex";
+      document.getElementById("gruposLiveLu1").style.justifyContent =
+        "space-between";
+      document.getElementById(
+        "btnAccionLu1"
+      ).innerHTML = `<a href="${linkLu1}" target="_blank">
+      <div><img src="img/ingresar.png" alt="" class="btn-action-img" /></div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu1"><p>Acceder</p></div>
+      </div>
+    </a>`;
+      document.getElementById("btnAccionLu1").style.backgroundColor = "#3EE45F";
+
+      // FINALIZADO
+      if (t.remainTime <= -5.0) {
+        clearInterval(timerUpdate);
+        console.log(t.remainTime);
+        el.innerHTML = "FINALIZADO";
+        document.getElementById("liveIDLu1").style.display = "none";
+        document.getElementById("gruposLiveLu1").style.justifyContent =
+          "space-around";
+        document.getElementById("btnAccionLu1").style.backgroundColor =
+          "#858585";
+        document.getElementById("btnAccionLu1").style.cursor = "no-drop";
+        document.getElementById("btnAccionLu1").innerHTML = ` <div>
+        <img class="btn-action-img" src="img/ingresar.png" alt="" />
+      </div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu1"><p>Finalizazdo</p></div>
+      </div>`;
+      document.getElementById("contenedorInfoLu1").className += " contenedor-informacion-off";
+      
+      }
+    }
+  }, 1000);
+};
+btnStateLu1("Oct 22 2021 12:17:10 GMT-05:00", "btnStateLu1", "Ingresar");
+
+const btnStateLu2 = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
+
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}d: ${t.remainHours}h: ${t.remainMinutes}m: ${t.remainSeconds}s `;
+    // EN VIVO
+    if (t.remainTime <= 1) {
+      console.log(t.remainTime);
+      // clearInterval(timerUpdate);
+      // el.innerHTML = finalMessage;
+    //   el.innerHTML = `<a href="http://itsm.edu.mx" target="_blank">
+    //   <p>Ingresar</p>
+    // </a>`;
+    el.innerHTML = `<p> Acceder <p>`;
+    document.getElementById("liveIDLu2").style.display = "flex";
+    document.getElementById("gruposLiveLu2").style.justifyContent =
+      "space-between";
+    document.getElementById(
+      "btnAccionLu2"
+    ).innerHTML = `<a href="${LinkLu2}" target="_blank">
+    <div><img src="img/ingresar.png" alt="" class="btn-action-img" /></div>
+    <div class="btn-action-estado">
+      <div class="btnState" id="btnStateLu2"><p>Acceder</p></div>
+    </div>
+  </a>`;
+    document.getElementById("btnAccionLu2").style.backgroundColor = "#3EE45F";
+
+    // FINALIZADO
+      if (t.remainTime <= -15.0) {
+        clearInterval(timerUpdate);
+        console.log(t.remainTime);
+        el.innerHTML = "FINALIZADO";
+        document.getElementById("liveIDLu2").style.display = "none";
+        document.getElementById("gruposLiveLu2").style.justifyContent =
+          "space-around";
+        document.getElementById("btnAccionLu2").style.backgroundColor =
+          "#858585";
+        document.getElementById("btnAccionLu2").style.cursor = "no-drop";
+        document.getElementById("btnAccionLu2").innerHTML = ` <div>
+        <img class="btn-action-img" src="img/ingresar.png" alt="" />
+      </div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu2"><p>Finalizazdo</p></div>
+      </div>`;
+      document.getElementById("contenedorInfoLu2").className += " contenedor-informacion-off";
+      }
+    }
+  }, 1000);
+};
+
+// btnStateLu2("Nov 15 2021 15:50:20 GMT-05:00", "btnStateLu2", "Ingresar");
+btnStateLu2("Oct 22 2021 12:18:20 GMT-05:00", "btnStateLu2", "Ingresar");
 
 
+const btnStateLu3 = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
 
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}d: ${t.remainHours}h: ${t.remainMinutes}m: ${t.remainSeconds}s `;
+
+    if (t.remainTime <= 1) {
+      console.log(t.remainTime);
+      // clearInterval(timerUpdate);
+      // el.innerHTML = finalMessage;
+      el.innerHTML = `<p> Acceder <p>`;
+      document.getElementById("liveIDLu3").style.display = "flex";
+      document.getElementById("gruposLiveLu3").style.justifyContent =
+        "space-between";
+      document.getElementById(
+        "btnAccionLu3"
+      ).innerHTML = `<a href="${linkLu1}" target="_blank">
+      <div><img src="img/ingresar.png" alt="" class="btn-action-img" /></div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu3"><p>Acceder</p></div>
+      </div>
+    </a>`;
+      document.getElementById("btnAccionLu3").style.backgroundColor = "#3EE45F";
+      if (t.remainTime <= -5.0) {
+        clearInterval(timerUpdate);
+        console.log(t.remainTime);
+        el.innerHTML = "FINALIZADO";
+        document.getElementById("liveIDLu3").style.display = "none";
+        document.getElementById("gruposLiveLu3").style.justifyContent =
+          "space-around";
+        document.getElementById("btnAccionLu3").style.backgroundColor =
+          "#858585";
+        document.getElementById("btnAccionLu3").style.cursor = "no-drop";
+        document.getElementById("btnAccionLu3").innerHTML = ` <div>
+        <img class="btn-action-img" src="img/ingresar.png" alt="" />
+      </div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu3"><p>Finalizazdo</p></div>
+      </div>`;
+      document.getElementById("contenedorInfoLu3").className += " contenedor-informacion-off";
+      }
+    }
+  }, 1000);
+};
+
+// btnStateLu3("Nov 15 2021 16:50:20 GMT-05:00", "btnStateLu3", "Ingresar");
+btnStateLu3("Oct 22 2021 12:19:20 GMT-05:00", "btnStateLu3", "Ingresar");
+
+const btnStateLu4 = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
+
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}d: ${t.remainHours}h: ${t.remainMinutes}m: ${t.remainSeconds}s `;
+    // EN VIVO
+    if (t.remainTime <= 1) {
+      console.log(t.remainTime);
+      // clearInterval(timerUpdate);
+      // el.innerHTML = finalMessage;
+      el.innerHTML = `<p> Acceder <p>`;
+      document.getElementById("liveIDLu4").style.display = "flex";
+      document.getElementById("gruposLiveLu4").style.justifyContent =
+        "space-between";
+      document.getElementById(
+        "btnAccionLu4"
+      ).innerHTML = `<a href="${linkLu1}" target="_blank">
+      <div><img src="img/ingresar.png" alt="" class="btn-action-img" /></div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu4"><p>Acceder</p></div>
+      </div>
+    </a>`;
+      document.getElementById("btnAccionLu4").style.backgroundColor = "#3EE45F";
+      // FINALIZADO 
+      if (t.remainTime <= -5.0) {
+        clearInterval(timerUpdate);
+        console.log(t.remainTime);
+        el.innerHTML = "FINALIZADO";
+        document.getElementById("liveIDLu4").style.display = "none";
+        document.getElementById("gruposLiveLu4").style.justifyContent =
+          "space-around";
+        document.getElementById("btnAccionLu4").style.backgroundColor =
+          "#858585";
+        document.getElementById("btnAccionLu4").style.cursor = "no-drop";
+        document.getElementById("btnAccionLu4").innerHTML = ` <div>
+        <img class="btn-action-img" src="img/ingresar.png" alt="" />
+      </div>
+      <div class="btn-action-estado">
+        <div class="btnState" id="btnStateLu4"><p>Finalizazdo</p></div>
+      </div>`;
+      document.getElementById("contenedorInfoLu4").className += " contenedor-informacion-off";
+      }
+    }
+  }, 1000);
+};
+
+// btnStateLu4("Nov 15 2021 17:50:20 GMT-05:00", "btnStateLu4", "Ingresar");
+btnStateLu4("Oct 22 2021 12:20:20 GMT-05:00", "btnStateLu4", "Ingresar");
